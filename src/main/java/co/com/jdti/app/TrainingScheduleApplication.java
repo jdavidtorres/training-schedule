fe43109d0d246c95500fc1aa29cd66761d6f47c5
jdavidtorres <jesu_ti91@hotmail.com>
2021-04-21 19:00:05 -0500

    Creacion de proyecto

---
+++ src/main/java/co/com/jdti/app/TrainingScheduleApplication.java
@@ -0,0 +1,12 @@
+package co.com.jdti.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingScheduleApplication.class, args);
+    }
+}
---
+++ src/main/java/co/com/jdti/app/configurations/DataSourceConfiguration.java
@@ -0,0 +1,38 @@
+package co.com.jdti.app.configurations;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource("classpath:application-schedule.properties")
+@EnableTransactionManagement
+public class DataSourceConfiguration {
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driver;
+
+    @Value("${spring.datasource.username}")
+    private String user;
+
+    @Value("${spring.datasource.password}")
+    private String pass;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName(driver);
+        dataSourceBuilder.url(url);
+        dataSourceBuilder.username(user);
+        dataSourceBuilder.password(pass);
+        return dataSourceBuilder.build();
+    }
+}
