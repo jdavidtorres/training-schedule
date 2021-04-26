f7fb3c9d95a7a8a486d024a648440f36d1b67f2e
jdavidtorres <jesu_ti91@hotmail.com>
2021-04-26 08:41:58 -0500

    Entidad Evento

--- src/main/java/co/com/jdti/app/models/entities/EventEntity.java
+++ src/main/java/co/com/jdti/app/models/entities/EventEntity.java
@@ -1,4 +1,46 @@
 package co.com.jdti.app.models.entities;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+@Entity
+@Table(name = "events")
+@Data
+@NoArgsConstructor
 public class EventEntity {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", unique = true, nullable = false, updatable = false)
+    private String id;
+
+    private String name;
+
+    @Column(name = "start_date", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date startDate;
+
+    @Column(name = "end_date", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date endDate;
+
+    private String description;
+
+    public EventEntity(String name, Date startDate, Date endDate, String description) {
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.description = description;
+    }
 }
