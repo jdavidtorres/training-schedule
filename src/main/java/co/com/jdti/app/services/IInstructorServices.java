3809861249f12476bd98a9613799e115ae976566
jdavidtorres <jesu_ti91@hotmail.com>
2021-04-26 08:42:38 -0500

    Servicios del componente instructor

--- src/main/java/co/com/jdti/app/services/IInstructorServices.java
+++ src/main/java/co/com/jdti/app/services/IInstructorServices.java
@@ -3,8 +3,15 @@ package co.com.jdti.app.services;
 import co.com.jdti.app.models.entities.InstructorEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IInstructorServices {
 
     List<InstructorEntity> findAllInstructors();
+
+    InstructorEntity save(InstructorEntity instructor);
+
+    InstructorEntity assignEvent(InstructorEntity instructorWithEvent);
+
+    Optional<InstructorEntity> findInstructorById(String idInstructor);
 }
--- src/main/java/co/com/jdti/app/services/InstructorServicesImpl.java
+++ src/main/java/co/com/jdti/app/services/InstructorServicesImpl.java
@@ -4,8 +4,10 @@ import co.com.jdti.app.models.entities.InstructorEntity;
 import co.com.jdti.app.models.repositories.IInstructorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class InstructorServicesImpl implements IInstructorServices {
@@ -13,8 +15,26 @@ public class InstructorServicesImpl implements IInstructorServices {
     @Autowired
     private IInstructorRepository iInstructorRepository;
 
+    @Transactional(readOnly = true)
     @Override
     public List<InstructorEntity> findAllInstructors() {
         return iInstructorRepository.findAll();
+    }
+
+    @Transactional
+    @Override
+    public InstructorEntity save(InstructorEntity instructor) {
+        return iInstructorRepository.save(instructor);
+    }
+
+    @Transactional
+    @Override
+    public InstructorEntity assignEvent(InstructorEntity instructorWithEvent) {
+        return iInstructorRepository.save(instructorWithEvent);
+    }
+
+    @Override
+    public Optional<InstructorEntity> findInstructorById(String idInstructor) {
+        return iInstructorRepository.findById(idInstructor);
     }
 }
