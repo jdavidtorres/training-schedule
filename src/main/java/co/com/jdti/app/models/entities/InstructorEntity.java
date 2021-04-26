b69370001cff5583e3d671db0ad383849d0cf9db
jdavidtorres <jesu_ti91@hotmail.com>
2021-04-26 08:41:45 -0500

    Cambiando los constructores requeridos

--- src/main/java/co/com/jdti/app/models/entities/InstructorEntity.java
+++ src/main/java/co/com/jdti/app/models/entities/InstructorEntity.java
@@ -1,20 +1,22 @@
 package co.com.jdti.app.models.entities;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "instructors")
 @Data
-@NoArgsConstructor
 public class InstructorEntity {
 
     @Id
@@ -31,4 +33,26 @@ public class InstructorEntity {
 
     @Column(nullable = false)
     private Date birthday;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<EventEntity> events;
+
+    public InstructorEntity() {
+        this.events = new ArrayList<>();
+    }
+
+    public InstructorEntity(String name, String lastname, Date birthday) {
+        this.name = name;
+        this.lastname = lastname;
+        this.birthday = birthday;
+        this.events = new ArrayList<>();
+    }
+
+    public InstructorEntity(String id, String name, String lastname, Date birthday) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        this.birthday = birthday;
+        this.events = new ArrayList<>();
+    }
 }
