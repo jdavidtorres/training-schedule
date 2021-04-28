0b4c53f492d05f5414c3d8543c3a1c0a02483936
jdavidtorres <jesu_ti91@hotmail.com>
2021-04-28 15:43:13 -0500

    Update InstructorServicesImpl.java

--- src/main/java/co/com/jdti/app/services/InstructorServicesImpl.java
+++ src/main/java/co/com/jdti/app/services/InstructorServicesImpl.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
@@ -50,14 +51,14 @@ public class InstructorServicesImpl implements IInstructorServices {
         var minutesT = 0L;
         List<EventEntity> events = instructor.getEvents();
         for (EventEntity event : events) {
-            var dateInit = event.getStart();
-            var instantInit = dateInit.toInstant();
-            var dateEnd = event.getEnd();
-            var instantEnd = dateEnd.toInstant();
-
-            long minutes = ChronoUnit.MINUTES.between(instantInit.atZone(ZoneId.systemDefault()).toLocalDateTime(), instantEnd.atZone(ZoneId.systemDefault()).toLocalDateTime().plusDays(1));
-
-            minutesT += minutes;
+            var dateInit = event.getStart().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+            var dateEnd = event.getEnd().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+            var month = dateInit.getMonth();
+            var actualMont = new Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime().getMonth();
+            if (month.equals(actualMont)) {
+                long minutes = ChronoUnit.MINUTES.between(dateInit, dateEnd.plusDays(1));
+                minutesT += minutes;
+            }
         }
         daysT = TimeUnit.DAYS.convert(minutesT, TimeUnit.MINUTES);
         return daysT;
